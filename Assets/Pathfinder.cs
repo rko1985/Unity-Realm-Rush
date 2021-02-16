992cc5154fa1a07d241e2482754e42df9cecb29c
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-02-16 09:24:31 -1000

    detecting neighbors of starting block

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -9,12 +9,35 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left,
+    };
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                // do nothing
+            }
+        }
     }
 
     private void ColorStartAndEnd()
