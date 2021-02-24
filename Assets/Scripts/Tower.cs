f8d54434a3f4ae7d48b2d46fa34c50214fe7cd2d
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-02-24 06:58:25 -1000

    tower points at enemy as it moves

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var who = gameObject;
+        objectToPan.LookAt(targetEnemy);
+    }
+}
