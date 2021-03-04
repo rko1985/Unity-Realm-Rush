189d46eeffce573ef501d39e15060729f795e319
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-03-03 15:42:47 -1000

    coroutine to spawn enemies

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true) //forever
+        {
+            print("Spawning");
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+        
+    }
+}
